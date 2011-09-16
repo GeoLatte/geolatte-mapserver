867ac8fbeb860f432297465914ace84887f97461
Jeroen Vuerinckx <jeroen.vuerinckx@qmino.com>
2011-09-16 13:21:39 +0200

    using the Tile Map Service Specification again for tests

--- src/test/java/org/geolatte/mapserver/tms/TestTileSetCoordinateSpace.java
+++ src/test/java/org/geolatte/mapserver/tms/TestTileSetCoordinateSpace.java
@@ -43,20 +43,20 @@ public class TestTileSetCoordinateSpace {
     public void test_get_bounds_for_tile_coordinate() {
 
         BoundingBox received = space1.boundingBox(TileCoordinate.valueOf(0, 0));
-        BoundingBox expected = new BoundingBox(500, 450, 550, 500);
+        BoundingBox expected = new BoundingBox(500, 500, 550, 550);
         assertEquals(expected, received);
 
         received = space1.boundingBox(TileCoordinate.valueOf(-1, -1));
-        expected = new BoundingBox(450, 500, 500, 550);
+        expected = new BoundingBox(450, 450, 500, 500);
         assertEquals(expected, received);
 
         received = space1.boundingBox(TileCoordinate.valueOf(2, 3));
-        expected = new BoundingBox(600, 300, 650, 350);
+        expected = new BoundingBox(600, 650, 650, 700);
         assertEquals(expected, received);
 
 
         received = space1.boundingBox(TileCoordinate.valueOf(100, 100));
-        expected = new BoundingBox(5500, -4550, 5550, -4500);
+        expected = new BoundingBox(5500, 5500, 5550, 5550);
         assertEquals("Also tilecoordinates beyond extent of tilemap are allowed.", expected, received);
 
     }
@@ -65,19 +65,19 @@ public class TestTileSetCoordinateSpace {
     public void test_get_pixelrange_for_tile_coordinate() {
         //TODO -- verify test cases
         PixelRange received = space1.tilePixelRange(TileCoordinate.valueOf(0, 0));
-        PixelRange expected = new PixelRange(90, 110, new Dimension(10, 10));
+        PixelRange expected = new PixelRange(90, 100, new Dimension(10, 10));
         assertEquals(expected, received);
 
         received = space1.tilePixelRange(TileCoordinate.valueOf(2, 3));
-        expected = new PixelRange(110, 140, new Dimension(10, 10));
+        expected = new PixelRange(110, 70, new Dimension(10, 10));
         assertEquals(expected, received);
 
         received = space1.tilePixelRange(TileCoordinate.valueOf(-9, 10));
-        expected = new PixelRange(0, 210, new Dimension(10, 10));
+        expected = new PixelRange(0, 0, new Dimension(10, 10));
         assertEquals(expected, received);
 
         received = space2.tilePixelRange(TileCoordinate.valueOf(0, 3));
-        expected = new PixelRange(0, 1792, new Dimension(256, 256));
+        expected = new PixelRange(0, 0, new Dimension(256, 256));
         assertEquals(expected, received);
 
 
@@ -117,16 +117,16 @@ public class TestTileSetCoordinateSpace {
         assertEquals(TileCoordinate.valueOf(0, 0), received);
 
         received = space1.tileCoordinateContaining(Point.valueOf(50, 50), true);
-        assertEquals(TileCoordinate.valueOf(-9, 9), received);
+        assertEquals(TileCoordinate.valueOf(-9, -9), received);
 
         received = space1.tileCoordinateContaining(Point.valueOf(610, 575), true);
-        assertEquals(TileCoordinate.valueOf(2, -1), received);
+        assertEquals(TileCoordinate.valueOf(2, 1), received);
 
         received = space1.tileCoordinateContaining(Point.valueOf(550, 550), false);
         assertEquals(TileCoordinate.valueOf(0, 0), received);
 
         received = space1.tileCoordinateContaining(Point.valueOf(550, 550), true);
-        assertEquals(TileCoordinate.valueOf(1, -1), received);
+        assertEquals(TileCoordinate.valueOf(1, 1), received);
 
         try {
             received = space2.tileCoordinateContaining(Point.valueOf(220, 220), true);
